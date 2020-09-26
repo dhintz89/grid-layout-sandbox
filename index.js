2ddcc61f63ce297c011be0571ec984c9cc09f730
dhintz <dhintz89@gmail.com>
2020-09-26 15:02:24 -0400

    add my logo to top of page

---
+++ index.js
@@ -0,0 +1,19 @@
+// const targetAry = document.querySelectorAll(".logo img");
+
+// function animate(tar) {
+//     anime({
+//         targets: tar,
+//         translateX: 250,
+//         rotateZ: 360,
+//         scale: 1.5,
+//         duration: 1000,
+//         delay: 500,
+//         endDelay: 500
+//     });
+// }
+
+// animate(targetAry);
+
+// animation.finished.then(function() {
+//     // next letter
+// });
