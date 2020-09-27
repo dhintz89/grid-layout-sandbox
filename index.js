27cce1cfb45cd1a3dd43be28ef8ba3734d3e89b7
dhintz <dhintz89@gmail.com>
2020-09-26 20:18:49 -0400

    logo text animation working besides ending cursor flash

--- index.js
+++ index.js
@@ -1,19 +1,136 @@
-// const targetAry = document.querySelectorAll(".logo img");
+// Wrap every letter in its own span
+const textWrapper = document.querySelectorAll('.letters');
+textWrapper.forEach((line, i) => {
+    let length = line.textContent.length;
+    line.innerHTML = line.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+});
 
-// function animate(tar) {
-//     anime({
-//         targets: tar,
-//         translateX: 250,
-//         rotateZ: 360,
-//         scale: 1.5,
-//         duration: 1000,
-//         delay: 500,
-//         endDelay: 500
-//     });
-// }
+function animate(thing) {
+    anime.timeline({loop: false})
+    // Daniel String
+    .add({
+        targets: '.cursor',
+        scaleX: [0,1],
+        opacity: [0.5,1],
+        easing: "linear",
+        duration: 150,
+        delay: 750
+    }).add({
+        targets: '.cursor',
+        translateX: [0, document.querySelector('.daniel').getBoundingClientRect().width + 6],
+        easing: "linear",
+        duration: 750,
+        delay: 200
+    }).add({
+        targets: '.daniel .letter',
+        opacity: 1,
+        easing: 'linear',
+        duration: 1,
+        offset: '-=900',
+        delay: (el, i) => 150 * (i+1)
+    }).add({
+        targets: '.cursor',
+        // scaleX: [1,0],
+        opacity: [1,0],
+        easing: "linear",
+        duration: 1,
+        delay: 100
+    })
+    // Hintz String
+    .add({
+        targets: '.cursor',
+        scaleX: [0,1],
+        opacity: [0.5,1],
+        easing: "linear",
+        duration: 150,
+        delay: 750
+    }).add({
+        targets: '.cursor',
+        translateX: [0, document.querySelector('.hintz').getBoundingClientRect().width + 5.6],
+        easing: "linear",
+        duration: 600,
+        delay: 200
+    }).add({
+        targets: '.hintz .letter',
+        opacity: [0,1],
+        easing: 'linear',
+        duration: 1,
+        offset: '-=750',
+        delay: (el, i) => 150 * (i+1)
+    }).add({
+        targets: '.cursor',
+        // scaleX: [1,0],
+        opacity: [1,0],
+        easing: "linear",
+        duration: 1,
+        delay: 100
+    })
+    // Designs String
+    .add({
+        targets: '.cursor',
+        scaleX: [0,1],
+        opacity: [0.5,1],
+        easing: "linear",
+        duration: 150,
+        delay: 750
+    }).add({
+        targets: '.cursor',
+        translateX: [0, document.querySelector('.designs').getBoundingClientRect().width + 5.6],
+        easing: "linear",
+        duration: 900,
+        delay: 200
+    }).add({
+        targets: '.designs .letter',
+        opacity: [0,1],
+        easing: 'linear',
+        duration: 1,
+        offset: '-=1050',
+        delay: (el, i) => 150 * (i+1)
+    }).add({
+        targets: '.cursor',
+        // scaleX: [1,0],
+        opacity: [1,0],
+        easing: "linear",
+        duration: 1,
+        delay: 100,
+    })
+    // cursor blinking effect
+    .add({
+        targets: '.cursor',
+        opacity: 1,
+        loop: 7,
+        easing: "linear",
+        duration: 150,
+        delay: 350,
+        direction: "alternate",
+    })
+};
 
-// animate(targetAry);
 
-// animation.finished.then(function() {
-//     // next letter
-// });
+// anime.timeline({loop: false}).add({
+//     targets: '.cursor',
+//     scaleX: [0,1],
+//     opacity: [0.5,1],
+//     easing: "linear",
+//     duration: 150,
+//     delay: function(el, i, l) {
+//         return i * 750;
+//     },
+//     endDelay: function(el, i, l) {
+//         return (l - i) * 100;
+//     }
+// })
+
+
+animate(document.querySelector('.letters'));
+
+
+    // anime({
+    //     targets: '.cursor',
+    //     opacity: 1,
+    //     loop: 7,
+    //     easing: "linear",
+    //     duration: 150,
+    //     delay: 350,
+    //     direction: "alternate",
+    // });
